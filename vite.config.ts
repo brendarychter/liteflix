3854553c12d66325089473b854929dd36b2d21d7
Brenda <brendarychter@gmail.com>
2023-08-18 19:55:53 -0300

    rollback env - add api key - add tag meta content security

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from "vite-plugin-svgr";
-import path from "path";
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svgr(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [svgr(), react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
     },
-  },
-})
+    define: {
+      __APP_ENV__: JSON.stringify(env.VITE_TMDB_API_KEY)
+    }
+  };
+});
