980388f2cb7aad840e1671fad78cb4913293b43a
Brenda <brendarychter@gmail.com>
2023-08-18 17:17:05 -0300

    rollback

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,3 @@
 export const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-export const BASE_PATH = '//api.themoviedb.org/3/movie';
-export const IMAGE_PATH = '//image.tmdb.org/t/p/original';
+export const BASE_PATH = 'https://api.themoviedb.org/3/movie';
+export const IMAGE_PATH = 'https://image.tmdb.org/t/p/original';
