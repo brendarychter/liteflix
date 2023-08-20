8f86395204bf980da69b872e5669d55d6b692533
Brenda <brendarychter@gmail.com>
2023-08-20 14:08:43 -0300

    cards with hover

--- src/components/Core/Card/index.tsx
+++ src/components/Core/Card/index.tsx
@@ -10,18 +10,25 @@ export const Card = ({
   return (
     <div className="card">
       <div className="overlay">
-        <div>
-
+        <div className="movie-name">
+          <span className="icon play-movie">
+            <Play />
+          </span>
+          <span>{title}</span>
+        </div>
+        <div className="movie-info">
+          <span className="icon star-movie">
+            <Star />
+            <span>{average}</span>
+          </span>
+          <span>{releaseYear}</span>
         </div>
       </div>
-      <img
-        className="path"
-        alt={title}
-        src={imagePath}
-        loading="lazy"
-      />
+      <img className="path" alt={title} src={imagePath} loading="lazy" />
       <div className="movie-description">
-        <span className="icon"><Play /></span>
+        <span className="icon">
+          <Play />
+        </span>
         <span className="movie-title">{title}</span>
       </div>
     </div>
--- src/pages/AddMovie/index.tsx
+++ src/pages/AddMovie/index.tsx
@@ -1,9 +1,19 @@
-function AddMovie() {
+import Modal from 'react-modal';
+
+const AddMovie = ({ isOpen, onRequestClose }: any) => {
   return (
-    <div>
-      Movie
-    </div>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      contentLabel="Example Modal"
+      className="custom-modal"
+      overlayClassName="custom-overlay"
+    >
+      <h2>Modal Content</h2>
+      <p>This is the content of the modal.</p>
+      <button onClick={onRequestClose}>Close</button>
+    </Modal>
   );
-}
+};
 
 export default AddMovie;
